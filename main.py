2e7eb948de17f083f4df44aa8f3f591aa2d61040
Nick  Murimi <126444800+NickMurimi@users.noreply.github.com>
2023-06-16 00:42:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print("---Fake Fan Finder---")
+favAnime = input("what's your favourite Anime? ")
+if favAnime == "one piece":
+  anyCharacter = input("oh, really?!Name me any of the characters? ")
+  if anyCharacter == "Nami":
+    jobRole = input("You got that by pure chance.okay then, what is her job on the ship? ")
+    if jobRole == "Navigator":
+      firstBounty = input("Hmph! what was her first bounty then? ")
+      if firstBounty == "Ummm...":
+        print("see! Fake one piece fan!")
+  
